dc272b1dab2991cc06821407ffdf81f1956d230e
Quoding <quoding@protonmail.ch>
2023-09-05 08:25:06 -0400

    Add tests

--- src/shapley_game/game.py
+++ src/shapley_game/game.py
@@ -2,13 +2,15 @@ from math import factorial
 from itertools import chain, combinations
 import logging
 
+
 def powerset(iterable):
     "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
     s = list(iterable)
-    return list(chain.from_iterable(combinations(s, r) for r in range(len(s)+1)))
+    return list(chain.from_iterable(combinations(s, r) for r in range(len(s) + 1)))
+
 
 class ShapleyGame:
-    def __init__(self, num_players:int, coal_scores:dict=None):
+    def __init__(self, num_players: int, coal_scores: dict = None):
         """Create a synthetic TU Game with its Shapley value solution
 
         Args:
@@ -17,37 +19,38 @@ class ShapleyGame:
         """
         self.num_players = num_players
         self.coal_scores = coal_scores
-        
-        if self.coal_scores is None:
-            self.coal_scores = self.generate_random_scores()
-            self.powerset = powerset(list(range(self.num_players)))
 
+        if self.coal_scores is None:
+            self.powerset = powerset(list(range(self.num_players)))
+            self.coal_scores = self.generate_random_scores()
         else:
             self.powerset = [tuple(k) for k in self.coal_scores.keys()]
 
-        self.compute_solution()
-
     def compute_solution(self):
         if self.num_players > 5:
-            logging.warning("Number of players is more than 5, this may take some time...")
-        
+            logging.warning(
+                "Number of players is more than 5, this may take some time..."
+            )
+
         # Create all permutations
         n_fact = factorial(self.num_players)
 
-        self.shapley_values = [None] * self.num_players
+        self.solution = [None] * self.num_players
 
         for player in range(self.num_players):
             phi = 0
             for coalition in self.powerset:
                 if player not in coalition:
-                    factor = factorial(len(coalition)) 
+                    factor = factorial(len(coalition))
                     factor *= factorial(self.num_players - len(coalition) - 1)
                     factor /= n_fact
 
-                    phi += factor * (self.coal_scores[frozenset(list(coalition) + [player])] - self.coal_scores[frozenset(coalition)])
-            
-            self.shapley_values[player] = phi
+                    phi += factor * (
+                        self.coal_scores[frozenset(list(coalition) + [player])]
+                        - self.coal_scores[frozenset(coalition)]
+                    )
 
+            self.solution[player] = phi
 
     def generate_random_scores(self):
         scores = [random.randint(0, 10) for i in range(len(self.powerset))]
@@ -56,5 +59,3 @@ class ShapleyGame:
         scores[0] = 0
 
         return {frozenset(self.powerset[i]): scores[i] for i in range(len(scores))}
-
-
---
+++ tests/test_sol.py
@@ -0,0 +1,27 @@
+import unittest
+import sys
+
+sys.path.append("../src/")
+
+from shapley_game.game import ShapleyGame, powerset
+
+
+class TestSol(unittest.TestCase):
+    def test_solution(self):
+        players = [0, 1, 2]
+        pset = powerset(players)
+
+        coalition_scores = {frozenset(coal): 0 for coal in pset}
+        coalition_scores[frozenset([0, 2])] = 1
+        coalition_scores[frozenset([1, 2])] = 1
+        coalition_scores[frozenset([0, 1, 2])] = 1
+        game = ShapleyGame(3, coalition_scores)
+        game.compute_solution()
+
+        solution = [1 / 6, 1 / 6, 2 / 3]
+
+        self.assertEqual(solution, game.solution)
+
+
+if __name__ == "__main__":
+    unittest.main()
