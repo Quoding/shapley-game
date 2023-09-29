70b106fc5eb30a67c9c69b7572390fd689375a2b
Quoding <quoding@protonmail.ch>
2023-09-29 15:21:18 -0400

    Properly rename imports in tests

--- tests/test_random.py
+++ tests/test_random.py
@@ -1,5 +1,5 @@
 import unittest
-from shapley_game.game import ShapleyGame, powerset
+from tu_games.game import ShapleyGame
 
 
 class TestSol(unittest.TestCase):
--- tests/test_sol.py
+++ tests/test_sol.py
@@ -1,5 +1,5 @@
 import unittest
-from shapley_game.game import ShapleyGame, powerset
+from tu_games.game import ShapleyGame, powerset
 
 
 class TestSol(unittest.TestCase):
